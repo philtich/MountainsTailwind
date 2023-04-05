12f2f926309da23e697f0e173e01aaf3b5d5dd0f
Philipp Tichy <philipptichy@hotmail.com>
2023-04-05 09:17:29 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.{html,js}'],
+  theme: {
+    
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#0077ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white': '#fff',
+      'black': '#000',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/img/großglockner.jpg')",
+      },
+      aspectRatio: {
+        '4/3': '4 / 3',
+      },
+      maxWidth: {
+        '1/2': '50%',
+      },
+      fontFamily: {
+        Opensans: ['OpenSansExtraBold', 'sans-serif'],
+        sansRegular: ['OpenSansRegular', 'sans-serif'],
+
+      },
+    },
+  },
+  plugins: [],
+}
+
